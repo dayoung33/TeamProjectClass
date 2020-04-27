d978e0304c28d44945f8e4d73be8f83b5affa34b
Dayoung Lee <rkek3766@gmail.com>
2020-04-28 00:24:18 +0900

    UI_HW
    sound on off
    pop-up on off

---
+++ 2D_UI_Class/Assets/Scripts/Button_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+   private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    public void myBottonScripct()
+    {
+        Debug.Log("hi");
+    }
+}
---
+++ 2D_UI_Class/Assets/Scripts/UI_Manager_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager_Script : MonoBehaviour
+{
+    private bool InitSetting;
+
+    // Start is called before the first frame update
+   private void Start()
+    {
+        InitSetting = false;
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        
+    }
+
+   public void MyLogInButton_in_UIManager()
+    {
+        Debug.Log("button 2 click");
+    }
+
+    public void SettingButton_UIManager(GameObject Setting)
+    {
+        if (!InitSetting)
+        {
+            Setting.SetActive(true);
+            InitSetting = true;
+        }
+    }
+
+    public void ExitButton(GameObject Setting)
+    {
+        Setting.SetActive(false);
+        InitSetting = false;
+    }
+
+    public void SoundMute(AudioSource audio)
+    {
+        audio.mute = true;
+    }
+    public void SoundOn(AudioSource audio)
+    {
+        audio.mute = false;
+    }
+}
